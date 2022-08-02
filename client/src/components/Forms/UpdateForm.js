60f12998e2bcb88480e0b5be9d6bd36dc48b6fb7
NiteshTiwari147 <64365692+NiteshTiwari147@users.noreply.github.com>
2022-08-02 15:05:40 +0530

    added modal to home pages

--- client/src/components/Forms/UpdateForm.js
+++ client/src/components/Forms/UpdateForm.js
@@ -1,33 +1,68 @@
 import React, { Component } from 'react';
+import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
 
+import EducationModal from "./educationModal";
+import ProjectModal from "./projectModal";
+import ExperienceModal from "./experienceModal";
+
 class UpdateForm extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {educationModalOpen: false,experienceModalOpen: false,projectModalOpen: false}
+    }
+
+    handleProjectModalOpen() {
+        this.setState({projectModalOpen: true})
+    }
+    handleProjectModalClose() {
+        this.setState({projectModalOpen: false})
+    }
+
+    handleExperienceModalOpen() {
+        this.setState({experienceModalOpen: true})
+    }
+    handleExperienceModalClose() {
+        this.setState({experienceModalOpen: false})
+    }
+
+    handleEducationModalOpen() {
+        this.setState({educationModalOpen: true})
+    }
+    handleEducationModalClose() {
+        this.setState({educationModalOpen: false})
+    }
     render() {
         return (
             <div className='updateForm container'>
                 <div className='updateCardContainer shadow'>
-                    <Link to="/projectForm" className='fillBox'>
-                        Add Project
-                    </Link>
+                    <Button size='medium' onClick={this.handleEducationModalOpen.bind(this)}>
+                        <div style={{fontSize: 'small', fontWeight: 'bold'}}>Add Education</div>
+                    </Button>
+                    <EducationModal open={this.state.educationModalOpen} close={this.handleEducationModalClose.bind(this)} />
                 </div>
                 <div className='updateCardContainer shadow'>
-                    <Link to="/educationForm" className='fillBox'>
-                        Add Education
-                    </Link>
+                    <Button size='medium' onClick={this.handleExperienceModalOpen.bind(this)}>
+                        <div style={{fontSize: 'small', fontWeight: 'bold'}}>Add Experience</div>
+                    </Button>
+                    <ExperienceModal open={this.state.experienceModalOpen} close={this.handleExperienceModalClose.bind(this)} />
                 </div>
                 <div className='updateCardContainer shadow'>
-                    <Link to="/experienceForm" className='fillBox'>
-                        Add Experience
-                    </Link>
+                    <Button size='medium' onClick={this.handleProjectModalOpen.bind(this)}>
+                        <div style={{fontSize: 'small', fontWeight: 'bold'}}>Add Project</div>
+                    </Button>
+                    <ProjectModal open={this.state.projectModalOpen} close={this.handleProjectModalClose.bind(this)} />
                 </div>
                 <div className='updateCardContainer shadow'>
-                    <Link to="/" className='fillBox'>
-                        Upload Certificate
-                    </Link>
+                    <Button size='medium' onClick={this.handleProjectModalOpen.bind(this)}>
+                        <div style={{fontSize: 'small', fontWeight: 'bold'}}>Add Certificate</div>
+                    </Button>
+                    <ProjectModal open={this.state.projectModalOpen} close={this.handleProjectModalClose.bind(this)} />
                 </div>
                 <div className='updateCardContainer shadow'>
                     <Link to="/dashboard" className='fillBox'>
-                        View Dashboard
+                            VIEW DASHBOARD
                     </Link>
                 </div>
             </div>
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -29,7 +29,7 @@ class Header extends Component {
             <nav className='navigationBar'>
                  <div className="navigationBarContent">
                     <div >
-                        <Link to='/dashboard' className="brand-logo companyLogo">
+                        <Link to='/home' className="brand-logo companyLogo">
                             HICLOUSIA
                         </Link>
                     </div>
