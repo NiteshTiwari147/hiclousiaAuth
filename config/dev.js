1461e99cb8e738d968734166a4e1f6c1d0cc1547
NiteshTiwari147 <64365692+NiteshTiwari147@users.noreply.github.com>
2022-12-08 12:25:38 +0530

    dev keys

---
+++ config/dev.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    googleClientID: '164357538139-sn0riqg3aom7htieestap44g5gffip0n.apps.googleusercontent.com',
+
+    googleClientSecret: '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',
+
+    mongoURI: 'mongodb+srv://nitesh:passwordnit@production-data.q2mkv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+
+    cookieKey: 'nvklsfbjkjsbvjbvlksbalksdVljablvjsdblkdshsnvodbvoeibs'
+
+};
